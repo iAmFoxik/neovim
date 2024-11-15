bbe9fc8b2b75a92286129af76f42285a36def45e
Foxik <grisha.bespyatyy@gmail.com>
2024-11-15 21:41:52 +0300

    update plugins

--- init.lua
+++ init.lua
@@ -1,35 +1,32 @@
------------
--- Установка Packer
------------
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+	  PACKER_BOOTSTRAP = fn.system({
+	    "git",
+	    "clone",
+	    "--depth",
+	    "1",
+	    "https://github.com/wbthomason/packer.nvim",
+	    install_path,
+	  })
+	  print("Installing packer close and reopen Neovim...")
+	  vim.cmd([[packadd packer.nvim]])
+	  return true
+else
+  return false
+	end
 end
 
------------------------------------------------------------
--- Импорт модулей lua
------------------------------------------------------------
+local packer_bootstrap = ensure_packer()
+
+if packer_bootstrap then
+    require('packer').sync()
+end
+if not packer_bootstrap then
 require('plugins')
 require('base')
 require('keys')
-
-if vim.g.vscode then
-  vim.cmd [[source $HOME/.config/nvim/vscode/settings.vim]]
-else
-  -- Настройка модулей
-  require('configs')
-
-  require('newpaper').setup()
+require('configs')
+require('newpaper').setup()
 end
