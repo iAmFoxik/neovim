75a6c309569e710c52339e3a0f681e7e3d1690e0
Foxik <grisha.bespyatyy@gmail.com>
2024-12-02 16:16:32 +0300

    update

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -7,12 +7,52 @@ return {
   },
   {
     'mrcjkb/rustaceanvim',
+    version = '^5',
+    lazy = false,
+    ft = "rust",
     config = function()
       vim.keymap.set('n', '<leader>i', function()
         local current_setting = vim.lsp.inlay_hint.is_enabled()
         vim.lsp.inlay_hint.enable(not current_setting)
       end)
+      local mason_registry = require('mason-registry')
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+      local cfg = require('rustaceanvim.config')
+
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
     end
+  },
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      require("dapui").setup()
+    end,
   },
   {
     'saecki/crates.nvim',
