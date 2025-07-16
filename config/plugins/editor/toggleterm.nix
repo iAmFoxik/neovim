85a4e2d32526d36a9c8a85f2b7e9c18ad8e18c21
Foxik <grisha.bespyatyy@gmail.com>
2025-07-16 22:44:06 +0300

    Updated: toggleterm

--- config/plugins/editor/toggleterm.nix
+++ config/plugins/editor/toggleterm.nix
@@ -2,12 +2,6 @@
   plugins.toggleterm = {
     enable = true;
     settings = {
-      direction = "float";
-      float_opts = {
-        border = "curved";
-        height = 30;
-        width = 130;
-      };
       open_mapping = "[[<c-\\>]]";
     };
   };
