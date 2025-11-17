d95767ca913154fc5d18fca34359e6304ee932d4
iAmFoxik <grep@mail.xyz>
2025-11-17 18:47:18 +0300

    Updated

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,15 @@
 return {
   "nvim-lualine/lualine.nvim",
+  -- dependencies = {
+  --   "SmiteshP/nvim-navic"
+  -- },
   config = function()
     local lualine = require("lualine")
 
     local function icon_filename()
       return [[filetype, filename]]
     end
+    -- local navic = require("nvim-navic")
 
     lualine.setup({
       options = {
@@ -25,14 +29,29 @@ return {
           }
         },
         lualine_b = {
-          { "filetype", icon_only = true, separator = "",                   padding = { left = 1, right = 0 } },
-          { "filename", separator = "",   padding = { left = 0, right = 1 } },
+          {
+            "filetype",
+            icon_only = true,
+            separator = "",
+            padding = { left = 1, right = 0 }
+          },
+          {
+            "filename",
+            separator = "",
+            padding = { left = 0, right = 1 }
+          },
           -- icon_filename
         },
         lualine_c = { "branch", {
           "diff",
           symbols = { added = " ", modified = " ", removed = " " }
-        } },
+        },
+          {
+            "navic",
+            color_correction = "dynamic",
+            navic_opts = nil,
+          },
+        },
         lualine_x = { "diagnostics" },
         lualine_y = { "lsp_status", "filetype" },
         lualine_z = { "location" },
